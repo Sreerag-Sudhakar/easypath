c0d78af40ef0d7c2fd02d448c18a60729e6d578a
Sreerag Sudhakar <sreeragsudhakar47@gmail.com>
2022-09-03 12:10:07 +0530

    Map loaded with a default location

--- easyPath.js
+++ easyPath.js
@@ -5,5 +5,16 @@
  * @description Loads the Google Maps
  */
 const loadMap = () => {
-    console.log("emit")
+    // The location of Ernakulam
+    const ernakulam = { lat: 9.9816, lng: 76.2999 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 9,
+        center: ernakulam,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
 }
